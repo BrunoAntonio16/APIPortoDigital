5a280afa63431014c95a5a797aaeca8bc4c2504b
aluno <aluno@embarquedigital.portodigital.local>
2024-03-07 14:28:08 -0300

    create-class-faculdade

---
+++ models/Faculdade.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SistemaEscolar.models
+{
+    public class Faculdade
+    {
+        public string Nome { get; set; }
+        public string CNPJ { get; set; }
+    }
+}
